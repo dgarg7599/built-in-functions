5e645ec23d218707b25e469e0a7e2de145454a41
dgarg7599 <gargdivyansh7599@gmail.com>
2025-03-07 21:39:38 +0530

    [Divyansh Garg] ADD: Implemented program to calculate GCD and LCM of two numbers using modular functions

---
+++ GCDandLCMCalculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class GCDandLCMCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        int gcd = findGCD(num1, num2);
+        int lcm = findLCM(num1, num2);
+
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + gcd);
+        System.out.println("LCM of " + num1 + " and " + num2 + " is: " + lcm);
+
+        scanner.close();
+    }
+
+    public static int findGCD(int num1, int num2) {
+        if (num2 == 0) {
+            return num1;
+        }
+        return findGCD(num2, num1 % num2);
+    }
+
+    public static int findLCM(int num1, int num2) {
+        return (num1 * num2) / findGCD(num1, num2);
+    }
+}
