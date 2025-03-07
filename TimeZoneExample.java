d202b48b08ef8e69f20ec9d19e067f12d31c2362
dgarg7599 <gargdivyansh7599@gmail.com>
2025-03-07 19:10:02 +0530

    [Divyansh Garg] ADD: Implemented program to display current time in multiple time zones using ZonedDateTime

---
+++ TimeZoneExample.java
@@ -0,0 +1,20 @@
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class TimeZoneExample {
+    public static void main(String[] args) {
+        // Get current time in different time zones
+        ZonedDateTime gmtTime = ZonedDateTime.now(ZoneId.of("GMT"));
+        ZonedDateTime istTime = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+        ZonedDateTime pstTime = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+
+        // Define a standard date-time formatter
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        // Display the current time in different time zones
+        System.out.println("Current time in GMT: " + gmtTime.format(formatter));
+        System.out.println("Current time in IST: " + istTime.format(formatter));
+        System.out.println("Current time in PST: " + pstTime.format(formatter));
+    }
+}
